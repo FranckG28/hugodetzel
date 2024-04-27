f027338c7c2bab07b3bcaf5d124d8f71c1137ac7
Franck <gutmann.franck@outlook.fr>
2024-04-27 18:12:32 +0200

    style: audio player

--- components/shared/AudioPlayer.tsx
+++ components/shared/AudioPlayer.tsx
@@ -13,7 +13,7 @@ export const AudioPlayer: FC<Props> = ({ playing, duration, className }) => {
   return (
     <div
       className={cn(
-        'bg-white/10 backdrop-blur-xl rounded-lg py-1.5 px-2 flex items-center gap-2 text-white text-xs',
+        'bg-white/10 backdrop-blur-xl rounded-lg py-1.5 px-2 flex items-center gap-2 text-white text-sm',
         className,
       )}
     >
